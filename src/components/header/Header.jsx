95537f1c22ca995d93dd3f6762ffae15e6a30e3c
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-02-05 16:07:48 -0300

    adding drop container into mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,68 +1,110 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import logo from '../../../public/logo.png';
 import Link from 'next/link';
+import DropContainer from '../drop-container/DropContainer';
 
-export default function Header() {
-  const secciones = [
-    {
-      seccion: 'inicio',
-      link: '/',
-    },
-    {
-    seccion: 'Institucional',
-    link: '/',
-    subsecciones: [
-      'Valores',
-      'Historia',
-      'Autoridades',
-      'Información Escolar'
-    ]
-  }, {
-    seccion: 'Niveles',
-    link: '/',
-    subsecciones: [
-      'Inicial',
-      'Primario',
-      'Secundaro'
-    ]
-  }, 
+const secciones = [
   {
-    seccion: 'Inscripción',
-    link: '/',
-  }, 
-  {
-    seccion: 'Administración',
+    name: 'inicio',
     link: '/',
   },
   {
-    seccion: 'Contacto',
+    name: 'Institucional',
+    link: '/',
+    subsecciones: [
+      {
+        name: 'Valores',
+        link: '/'
+      }, {
+        name: 'Historia',
+        link: '/'
+      }, {
+        name: 'Autoridades',
+        link: '/'
+      }, {
+        name: 'Información Escolar',
+        link: '/'
+      }
+    ]
+  }, {
+    name: 'Niveles',
+    link: '/',
+    subsecciones: [
+      {
+        name: 'Inicial',
+        link: '/',
+        subsecciones: [
+          {
+            name: 'Inicial',
+            link: '/'
+          }, {
+            name: 'Primario',
+            link: '/'
+          }, {
+            name: 'Secundario',
+            link: '/'
+          }
+        ]
+      }, {
+        name: 'Primario',
+        link: '/'
+      }, {
+        name: 'Secundario',
+        link: '/'
+      }
+    ]
+  },
+  {
+    name: 'Inscripción',
+    link: '/',
+  },
+  {
+    name: 'Administración',
+    link: '/',
+  },
+  {
+    name: 'Contacto',
     link: '/',
   }
 ];
 
+export default function Header() {
+  const [hamburger, setHamburger] = useState(false);
+
+  const handleHamburger = () => {
+    setHamburger(!hamburger)
+  }
+
   return (
-    <header className='fixed top-0 w-full h-auto bg-white'>
+    <header className='fixed top-0 w-full h-auto bg-[#fff]'>
       <div className="hidden md:flex h-[46px] w bg-[var(--bg)]"></div>
       <div className="h-[120px] md:h-[66px] w shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] flex items-center justify-start">
-      
-        <div className="flex-center relative h w-[120px] md:w-[300px]">
+
+        <div className="flex-center relative h w-[200px] md:w-[300px]">
           <Link className='md:absolute top-[-35px]' href="/">
-            <Image src={logo} alt='logo' className='min-w-[85%] h-auto' />
+            <Image src={logo} alt='logo' className='w-[100px] md:min-w-[85%] h-auto' />
           </Link>
         </div>
 
-        <label className="hamburger-menu">
-          <input type="checkbox" />
-        </label>
+        <div className="md:hidden w flex items-center justify-end">
+          <div className="w-[30%] flex-center">
+            <label className="hamburger-menu">
+              <input type="checkbox" onClick={handleHamburger} />
+            </label>
+          </div>
+        </div>
+
 
         <div className="h w-[90%] hidden md:flex">
-          <nav className='justify-center h w-[80%]'>
-            <ul className='flex-center flex-row h'>
+          <nav className="justify-center h w-[80%]">
+            <ul className="flex-center flex-row h">
               {
-                secciones.map(({link, seccion, subsecciones}) => (
-                  <li key={seccion} className='h-[80%] flex-center'>
-                    <Link href={link} className='text-[1.2rem] px-5 h flex-center hover:border-b-[3px] border-[var(--bg)]'>
+                secciones.map(({ link, seccion, subsecciones }) => (
+                  <li key={seccion} className="h-[80%] flex-center">
+                    <Link href={link} className="text-[1.2rem] px-5 h flex-center hover:border-b-[3px] border-[var(--bg)]">
                       {seccion}
                     </Link>
                   </li>
@@ -72,6 +114,14 @@ export default function Header() {
           </nav>
         </div>
       </div>
+
+      {hamburger &&
+        <div className="relative">
+          <nav className="absolute w-full z-[2] max-h-[50vh] overflow-y-auto">
+            <DropContainer secciones={secciones} />
+          </nav>
+        </div>
+      }
     </header>
   )
 }
